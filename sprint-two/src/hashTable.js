dd6fc655c9dd254ebdf274f17cde26d8e773361e
Allan Trinh <daigortrinh@gmail.com>
2015-11-19 17:00:45 -0800

    implement set and attempt hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,22 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, k);
+  this._storage.set(i[])
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var index = this._storage.get(i);
+  for(var i = 0; i < index.length; i++){
+    if(index[i][0] === k){
+      return index[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  this.insert(k, null);
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this.contains(item)){
+    this.storage[item] = true;
+  }
 };
 
 setPrototype.contains = function(item){
+  for(var key in this.storage){
+    if(item === key){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this.storage[item];
 };
 
 /*
