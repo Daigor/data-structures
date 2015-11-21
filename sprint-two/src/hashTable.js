937581624379001024779df5509d78ebe8c41880
Allan Trinh <daigortrinh@gmail.com>
2015-11-20 19:20:02 -0800

    removed console log testing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -30,7 +30,6 @@ HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
  
   var index = this._storage.get(i);
-  console.log(index, 'im in retrieve / index')
   for(var j = 0; j < index.length; j++){
     if(index[j][0] === k){
       return index[j][1];
@@ -44,10 +43,8 @@ HashTable.prototype.remove = function(k){
   for(var j = 0; j < index.length; j++){
     if(index[j][0] === k){
       index[j][1] = null;
-      console.log(index, 'inside for loop of remove');
     }
   }
-  console.log(this._limit, 'remove function');
   if(this.count / this._limit <= .25){
     //debugger;
     this._limit = this._limit / 2;
@@ -62,10 +59,8 @@ HashTable.prototype.resize = function(){
   var newStorage = LimitedArray(this._limit);
   this._storage.each(function(array, key){
     if(array){
-      console.log(array, 'im array in resize')
       for(var i = 0; i < array.length; i++){
         var tuple = array[i];
-        console.log(tuple, 'im tuple');
         var newIndex = getIndexBelowMaxForKey(tuple[0], saveLimit);
         var place = newStorage.get(newIndex) || [];
         place.push([tuple[0], tuple[1]]);
